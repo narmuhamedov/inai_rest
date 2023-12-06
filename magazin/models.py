965d01de8042b98dd610bb471a3fdd53b4b733d3
rzpy1997 <kingradomir@gmail.com>
2023-12-06 15:24:23 +0600

    'lesson2json'

--- magazin/models.py
+++ magazin/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -7,6 +14,16 @@ class Product(models.Model):
     price = models.FloatField()
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+
+    def __str__(self):
+        return self.text
--- magazin/serializers.py
+++ magazin/serializers.py
@@ -1,9 +1,24 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Category, Review
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    reviews = ReviewSerializer(many=True)
+
     class Meta:
         model = Product
         # fields = '__all__'
-        fields = 'id title created_at'.split()
+        fields = 'id title created_at category reviews'.split()
